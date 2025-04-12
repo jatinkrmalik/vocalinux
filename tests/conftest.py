e8d4732de4c6253a332a46774d423d7207c31e37
jatinkrmalik <jatinkrmalik@gmail.com>
2025-04-12 02:15:04 -0500

    Fix test imports by setting PYTHONPATH and adding conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+"""
+Configuration file for pytest.
+This file makes sure that the 'src' module can be imported in tests.
+"""
+import os
+import sys
+
+# Add the parent directory to sys.path so that 'src' can be imported
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
