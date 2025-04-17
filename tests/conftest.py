fe628faafbe6fe91b2b7b40f59ed0fd2909af01d
jatinkrmalik <jatinkrmalik@gmail.com>
2025-04-16 19:46:57 -0500

    Restructure tests for better organization and maintainability

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,6 @@ import sys
 
 # Add the parent directory to sys.path so that 'src' can be imported
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# This will help pytest discover all test files correctly
+pytest_plugins = []
