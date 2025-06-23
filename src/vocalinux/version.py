7a283169b8a6bede18cebe16e8909c79c9d76c24
Jatin K Malik <jatinkrmalik@gmail.com>
2025-06-23 15:02:51 -0500

    Improve installation scripts with better platform detection, dependency management, and error handling (#29)

---
+++ src/vocalinux/version.py
@@ -0,0 +1,9 @@
+"""
+Version information for Vocalinux.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Jatin K Malik"
+__email__ = "jatinkrmalik@gmail.com"
+__license__ = "GPL-3.0"
+__copyright__ = "Copyright 2023-2024 Jatin K Malik"
