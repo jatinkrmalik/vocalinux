d31dab6fd2badb16dbf9d7afb45f788c4758aee9
jatinkrmalik <jatinkrmalik@gmail.com>
2026-01-20 00:00:28 -0800

    chore: update copyright year to 2026

--- src/vocalinux/version.py
+++ src/vocalinux/version.py
@@ -7,6 +7,6 @@ __version_info__ = (0, 2, 0, "alpha")
 __author__ = "Jatin K Malik"
 __email__ = "jatinkrmalik@gmail.com"
 __license__ = "GPL-3.0"
-__copyright__ = "Copyright 2023-2025 Jatin K Malik"
+__copyright__ = "Copyright 2023-2026 Jatin K Malik"
 __url__ = "https://github.com/jatinkrmalik/vocalinux"
 __description__ = "A seamless voice dictation system for Linux"
